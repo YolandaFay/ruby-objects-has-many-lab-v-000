ebcbd573ea2b34cfed913a18c3c37c626ce3526b
Yolanda Fay <yolandafay@gmail.com>
2019-03-06 03:24:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,35 @@
-class Author 
+class Author  
   
   attr_accessor :name 
+  attr_reader :posts  
+  
+  @@song_count = 0 
+  
+  def initialize (name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs()
+    @songs
+  end
+  
+  def add_song(song)
+    @@song_count += 1 
+    @songs << song #has many 
+    song.artist = self  #belongs to
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.add_song(song)
+    song 
+    #@songs << song
+    #song.artist = self 
+  end
+  
+  def self.song_count()
+    @@song_count 
+  end
 
-  
-  
-  
-  
 end
