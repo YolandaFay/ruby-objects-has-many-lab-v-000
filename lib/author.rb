b318921695674d29e1cd91578e9e62ffc2b86bc5
Yolanda Fay <yolandafay@gmail.com>
2019-03-06 03:26:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,12 @@
 class Author  
   
-  attr_accessor :name 
+  attr_accessor :title  
   attr_reader :posts
   
   @@post_count = 0 
   
-  def initialize (name)
-    @name = name
+  def initialize (title)
+    @title = title
     @posts = []
   end
   
@@ -20,7 +20,7 @@ class Author
     post.artist = self  #belongs to
   end
   
-  def add_song_by_name(name)
+  def add_post_by_title(title)
     song = Song.new(name)
     self.add_song(song)
     song 
