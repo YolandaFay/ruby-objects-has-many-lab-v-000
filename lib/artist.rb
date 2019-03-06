d93b72a50896d549af428602f4f88187334192fd
Yolanda Fay <yolandafay@gmail.com>
2019-03-06 02:47:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
   end
   
   def add_song(song)
-    @songs << song 
-    song.artist = self 
+    @songs << song #has many 
+    song.artist = self  #belongs to
   end
   
   def add_song_by_name(name)
